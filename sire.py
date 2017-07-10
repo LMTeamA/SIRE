3b2614e5ec9d970ad929bbfc2fba54510ce83d85
vixadd <dkroell@acm.org>
2017-07-10 04:24:41 -0400

    initial commit

---
+++ sire.py
@@ -0,0 +1,15 @@
+#   
+#   Image API used in SonoBouy
+#   David Kroell
+#
+
+from PIL import Image, ImageTk
+
+class Image():
+    def __init__(self, path):
+        self.image_path = path
+        self.image_mutations = []
+
+        
+        
+        
